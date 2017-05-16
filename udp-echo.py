8a88bf9f4b6fd99b26e49662a51ffe7d02b7f209
tj8000rpm <t.j.8000rpm@gmail.com>
2017-05-17 01:37:35 +0900

    add comment and etc

--- udp-echo.py
+++ udp-echo.py
@@ -9,26 +9,34 @@ from contextlib import closing
 def main(args):
 
   bufsize = 4096
-  print(args)
-  print(len(args))
+
+  # 引数に設定がある場合は引数を使う
+  # 第一引数:受信ipアドレス
+  # 第二引数:受信ポート
   if len(args) == 3 :
     localhost=args[1]
     localport=int(args[2])
-  else :
-    localhost = '10.0.0.1'
+  else : # 引数がない場合はループバックアドレス等を設定
+    localhost = '127.0.0.1'
     localport = 6000
-
+  print('Started listening ' + localhost + ':' + str(localport) )
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   with closing(sock):
     sock.bind((localhost, localport))
     while True:
+      #バッファサイズ内のデータを取得
+      #return : (bytes,address)
       recvobj=sock.recvfrom(bufsize)
+      #udp payload取得
       message=recvobj[0]
-      remotes=recvobj[1]
-      remotehost=remotes[0]
-      remoteport=remotes[1]
-      print(message)
-      print(remotes)
+      #送信元アドレス取得
+      address=recvobj[1]
+      
+      #送信元アドレス取得
+      remotehost=address[0]
+      remoteport=address[1]
+      #print(message)
+      #print(remotes)
       sock.sendto(message,(remotehost,remoteport))
   return
 
